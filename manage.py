9b65e8bb773b0772b05c5d29d05e428b37e335a6
murathe <isaacsm07p@gmail.com>
2020-01-20 14:10:46 +0300

    Create app instance

--- manage.py
+++ manage.py
@@ -2,3 +2,6 @@ from app import create_app,db
 from flask_script import Manager, Server
 from flask_migrate import MIgrate, MigrateCommand
 from app.model import User, Comment, POst, Quote
+
+#Create app instance
+app = create_app('development')
