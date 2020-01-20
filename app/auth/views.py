2b35e76f1341ee4299ce2597c38107ae4c60297c
murathe <isaacsm07p@gmail.com>
2020-01-20 09:17:48 +0300

    Major package imports

---
+++ app/auth/views.py
@@ -0,0 +1,8 @@
+from . import auth
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, login_required
+from ..models import User
+from forms import SignupForm, LoginForm
+from .. import db
+from ..email import mail_message
+import markdown2
