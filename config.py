b5ad3ee3da4baaf5a2cf681a1c0e34605f18d359
murathe <isaacsm07p@gmail.com>
2020-01-20 14:02:52 +0300

    dEFINE prodconfig class

--- config.py
+++ config.py
@@ -5,3 +5,19 @@ class Config:
     General - parent configuration class
     '''
     QUOTE_API_KEY_BASE = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get('secret_key')
+    UPLOADEDE_PHOTO_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    SQLALCHEMY_TRACK_MODIFICATION = False
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://<enterpd>:<enterpd>@localhost/<enterbf>'
+
+
+class ProdConfig(Config):
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
