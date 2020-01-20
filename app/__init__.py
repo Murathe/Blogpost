09e17fcad3a0150e7ba281275d9770a08e2a85bb
murathe <isaacsm07p@gmail.com>
2020-01-20 12:18:46 +0300

    Init extensions

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,15 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask_simplemde import Simplemde
+from flask_simplemde import SimpleMDE
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+simple = SimpleMDE()
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
