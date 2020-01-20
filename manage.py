ac563ee8480c2495938646ddf8d98f0c7f443b4b
murathe <isaacsm07p@gmail.com>
2020-01-20 14:13:52 +0300

    Pre-define test class

--- manage.py
+++ manage.py
@@ -1,7 +1,22 @@
 from app import create_app,db
 from flask_script import Manager, Server
-from flask_migrate import MIgrate, MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 from app.model import User, Comment, POst, Quote
 
 #Create app instance
 app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+
+migrate Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+@manger.command
+def test():
+    '''
+    Run unittests
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    
