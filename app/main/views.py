154fae48c312c591243358208384b08610b0803e
murathe <isaacsm07p@gmail.com>
2020-01-20 11:08:43 +0300

    Comments delete func

--- app/main/views.py
+++ app/main/views.py
@@ -14,3 +14,33 @@ def index():
    
    return render_template('index.html', categories=categories, quotes=random_quote)
 
+@main.route('/blog/<string:category>')
+def posts(category):
+   posts = list(Post.query.filter_by(category=category))
+   return render_template('/blog.html', posts=posts)
+
+@main.route('/post', methods=['GET', 'POST'])
+@login_required
+def add():
+    post_form=PostForm()
+    if post_form.validate_on_submit():
+        post=post(title=post_form.title.data, text=post_form.data, category=post_form.category.data, user_id=current_user.id, username=current_user.username)
+
+        post.save_post()
+        return redirect(url_for('main.index'))
+
+    return render_template('./post.html', post_form=post_form)
+
+@main.route('/delete/<int:id>', methods['GET', 'POST'])
+@login_required
+def delete(id):
+    deleted = Post.query.filter_by(id=id).first()
+    db.session.delete(deleted)
+    db.session.commit()
+
+    return redirect(url_for('main.index.html'))
+
+
+
+@main.route('/comment/new/<int:post_id>', methods=['GET','POST'])
+@login_required
