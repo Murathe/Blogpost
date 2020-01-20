a92f6633fd6b63c185af246a454aabfa1df9ab3b
murathe <isaacsm07p@gmail.com>
2020-01-20 10:06:15 +0300

    comment functionality class

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,10 +4,12 @@ from wtforms.validators import Required, Length, DataRequired
 
 
 class PostForm(FlaskForm):
-    CATEGORIES=[('ART','ART'),('FINANCE','FINANCE'),('GAMING','GAMING'),('MUSIC','MUSIC')]
-    category=SelectField('CATEGORIES',choices=CATEGORIES)
-    titles=StringField('TITLE', validators=[DataRequired()])
-    post=TextAreaField('BLOG', VALIDATORS=[Required()])
-    submit=SubmitField('Publish')
+    CATEGORIES= [('ART','ART'),('FINANCE','FINANCE'),('GAMING','GAMING'),('MUSIC','MUSIC')]
+    category = SelectField('CATEGORIES',choices=CATEGORIES)
+    titles = StringField('TITLE', validators=[DataRequired()])
+    post = TextAreaField('BLOG', VALIDATORS=[Required()])
+    submit = SubmitField('Publish')
 
-    
+class CommentForm(FlaskForm):
+    comment = TextAreaField('')
+    submit = SubmitField('Submit')
