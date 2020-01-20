50f5a8799047a50e2fb78110f36c56e133910a1e
murathe <isaacsm07p@gmail.com>
2020-01-20 10:02:11 +0300

    List categories(dict)

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,3 +3,6 @@ from wtforms import StringField, SelectField, TextAreaField, SubmitField
 from wtforms.validators import Required, Length, DataRequired
 
 
+class PostForm(FlaskForm):
+    CATEGORIES=[('ART','ART'),('FINANCE','FINANCE'),('GAMING','GAMING'),('MUSIC','MUSIC')]
+    
