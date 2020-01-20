a1deb8e7ce466dbef06a2b3602a6bfea7793e22f
murathe <isaacsm07p@gmail.com>
2020-01-20 10:05:01 +0300

    POst class

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,4 +5,9 @@ from wtforms.validators import Required, Length, DataRequired
 
 class PostForm(FlaskForm):
     CATEGORIES=[('ART','ART'),('FINANCE','FINANCE'),('GAMING','GAMING'),('MUSIC','MUSIC')]
+    category=SelectField('CATEGORIES',choices=CATEGORIES)
+    titles=StringField('TITLE', validators=[DataRequired()])
+    post=TextAreaField('BLOG', VALIDATORS=[Required()])
+    submit=SubmitField('Publish')
+
     
