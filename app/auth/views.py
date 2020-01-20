1b27f2913ddb3414db23a12537c43d4845f56472
murathe <isaacsm07p@gmail.com>
2020-01-20 09:25:42 +0300

    Define login route

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,3 +6,17 @@ from forms import SignupForm, LoginForm
 from .. import db
 from ..email import mail_message
 import markdown2
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login();
+    login_form=LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.emial.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Wrong Username or Password')
+
+    title = "Login"
+    return render_template('auth/login.html', login_form = login_form, title = title)
