9925b96d86b60b22c245ed32f8c62a537941bc11
murathe <isaacsm07p@gmail.com>
2020-01-20 09:01:05 +0300

    Define signup form class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,3 +2,6 @@ from flask_wtf import FlaskForm
 from wtfforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Email, EqualTo
 from wtforms import ValidateError 
+
+class SignupForm(FlaskForm):
+    
